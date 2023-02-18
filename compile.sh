bf87e1ce60d71b1e8ccaf30339b0dfd0d9cb6f94
manosanaggh <manos.anagnostakis@windowslive.com>
2023-02-18 13:27:22 +0200

    [feat] Added compile.sh and run.sh with tests.

---
+++ compile.sh
@@ -0,0 +1,7 @@
+bash ./configure \
+--with-boot-jdk=/usr \
+--with-debug-level=release \
+--with-target-bits=64 \
+--with-jobs=4
+
+make
---
+++ manos_tests/run.sh
@@ -0,0 +1,2 @@
+../build/linux-x86_64-server-release/jdk/bin/javac HelloWorld.java
+../build/linux-x86_64-server-release/jdk/bin/java HelloWorld
